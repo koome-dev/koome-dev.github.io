04e678502231687ac0cfba1938a06b9ca8b81ffb
Koome Derrick <138670931+koome-dev@users.noreply.github.com>
2024-08-02 17:38:10 +0300

    add files via upload

---
+++ data/point.js
@@ -0,0 +1,55 @@
+var pointJson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        Name: "Point",
+      },
+      geometry: {
+        coordinates: [36.707988592123684, -1.2838866911755957],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        coordinates: [36.89551390268687, -1.2546235090336921],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        coordinates: [36.91603838410302, -1.3819411619004853],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        coordinates: [36.74447105421882, -1.3921011527035603],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        coordinates: [36.96738046397067, -1.4879764463056233],
+        type: "Point",
+      },
+    },
+    {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        coordinates: [36.82033935484972, -1.276047321418389],
+        type: "Point",
+      },
+    },
+  ],
+};
